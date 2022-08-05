a278ee35d53bf67f4bfb92944345e92f942bef64
Dan Langan <danlangan@Dans-MBP-2.hsd1.il.comcast.net>
2022-08-05 13:03:06 -0500

    edited all of the trait search functions, have yet to set the variable for found person by trait at the end of the function. doing this commit now to see what the instructors think for how I should set that up

--- app.js
+++ app.js
@@ -120,10 +120,10 @@ function searchByTrait(people) {
     let searchTraitOptions = promptFor(
         `Select and type each trait(s) that you would you like to seach for:\n\n
         'gender'\n'height'\n'weight'\n'eyeColor'\n'occupation\n'
-        `)
+        `, chars);
 
         if (searchTraitOptions === "gender") {
-            let genderSearchOption = promptFor("Would you like to search for males for females?")
+            let genderSearchOption = promptFor("Would you like to search for males for females?", chars);
             
             if (genderSearchOption === "female") {
                 (people.filter(function(people){(person.gender)}) === "female")
@@ -131,14 +131,15 @@ function searchByTrait(people) {
                     person.gender =+ genderSearchResults
                 }
                 return genderSearchResults
-            }
-            else (genderSearchOption === "male") {
+            };
+            if (genderSearchOption === "male") {
                 (people.filter(function(people){(person.gender)}) === "male")
                 for ("male" in person.gender) {
                     person.gender =+ genderSearchResults
                 }
                 return genderSearchResults
             };
+            searchByTrait();
         };
         if (searchTraitOptions === "height") {
             let heightSearchOption = parseInt(promptFor("How tall (in inches) do you think the person that you are looking for is? "))
@@ -149,39 +150,143 @@ function searchByTrait(people) {
                     person.height =+ heightSearchResults
                 }
                 return heightSearchResults
-            }
-            else (heightSearchOption > 69) {
+            };
+            if (heightSearchOption > 69) {
                 (people.filter(function(people){(person.height)}) > 69)
                 for (height in person.height) {
                     person.height =+ heightSearchResults
                 }
                 return heightSearchResults
-            }
+            };
+            seatchByTrait();
         };
+        if (searchTraitOptions === "weight") {
+            let weightSearchOption = parseInt(promptFor("How much do you think this person weighs? "))
 
-        switch (searchTraitOptions) {
-            case "gender":
-                let genderSearchResults = promptFor("What's this persons gender? Enter n/a if you are not sure.", chars)
-                switch (genderSearchResults) {}
-            case "height":
-                promptFor("How tall is this person? Enter n/a if you are not sure.", chars);
-                return height;
-            case "weight":
-                promptFor("How much does this person weigh? Enter n/a if you are not sure.", chars);
-                return weight;
-            case "eyeColor":
-                promptFor("What is this person's eye color? Enter n/a if you are not sure.", chars);
-                return eyeColor;
-            case "occupation":
-                promptFor("What job does this person have? Enter n/a if you are not sure.", chars);
-                return occupation;
-        
+            if (weightSearchOption < 170) {
+                (people.filter(function(people){(person.weight)}) < 170)
+                for (weight in person.weight) {
+                    person.weight =+ weightSearchResults
+                }
+                return weightSearchResults
+            };
+            if (weightSearchOption > 169) {
+                (people.filter(function(people){(person.weight)}) > 169)
+                for (weight in person.weight) {
+                    person.weight =+ weightSearchResults
+                }
+                return heightSearchResults
+            };
+            searchByTrait();
         };
+        if (searchTraitOptions === "eyecolor") {
+            let eyeColorSearchOption = promptFor("What color eyes does this person have?", chars);
+            
+            if (eyeColorSearchOption === "black") {
+                (people.filter(function(people){(person.eyeColor)}) === "black")
+                for ("black" in person.eyeColor) {
+                    person.eyeColor =+ eyeColorSearchResults
+                }
+                return eyeColorSearchResults
+            };
+            if (eyeColorSearchOption === "brown") {
+                (people.filter(function(people){(person.eyeColor)}) === "brown")
+                for ("brown" in person.eyeColor) {
+                    person.eyeColor =+ eyeColorSearchResults
+                }
+                return eyeColorSearchResults
+            };
+            if (eyeColorSearchOption === "hazel") {
+                (people.filter(function(people){(person.eyeColor)}) === "hazel")
+                for ("hazel" in person.eyeColor) {
+                    person.eyeColor =+ eyeColorSearchResults
+                }
+                return eyeColorSearchResults
+            };
+            if (eyeColorSearchOption === "blue") {
+                (people.filter(function(people){(person.eyeColor)}) === "blue")
+                for ("blue" in person.eyeColor) {
+                    person.eyeColor =+ eyeColorSearchResults
+                }
+                return eyeColorSearchResults
+            };
+            if (eyeColorSearchOption === "green") {
+                (people.filter(function(people){(person.eyeColor)}) === "green")
+                for ("green" in person.eyeColor) {
+                    person.eyeColor =+ eyeColorSearchResults
+                }
+                return eyeColorSearchResults
+            };
+            searchByTrait();
+        };
+        if (searchTraitOptions === "occupation") {
+            let occupationSearchOption = promptFor("What job does this person have?", chars);
+            
+            if (occupationSearchOption === "programmer") {
+                (people.filter(function(people){(person.occupation)}) === "programmer")
+                for ("programmer" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            if (occupationSearchOption === "assistant") {
+                (people.filter(function(people){(person.occupation)}) === "assistant")
+                for ("assistant" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            if (occupationSearchOption === "landscaper") {
+                (people.filter(function(people){(person.occupation)}) === "landscaper")
+                for ("landscaper" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            if (occupationSearchOption === "nurse") {
+                (people.filter(function(people){(person.occupation)}) === "nurse")
+                for ("nurse" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            if (occupationSearchOption === "student") {
+                (people.filter(function(people){(person.occupation)}) === "student")
+                for ("student" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            if (occupationSearchOption === "architect") {
+                (people.filter(function(people){(person.occupation)}) === "architect")
+                for ("architect" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            if (occupationSearchOption === "doctor") {
+                (people.filter(function(people){(person.occupation)}) === "doctor")
+                for ("doctor" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            if (occupationSearchOption === "politician") {
+                (people.filter(function(people){(person.occupation)}) === "politician")
+                for ("politician" in person.occupation) {
+                    person.occupation =+ occupationSearchResults
+                }
+                return occupationSearchResults
+            };
+            searchByTrait();
+
     // Listed are all of the traits that someone could search by if they do not know the persons name.
-    let foundPeopleByTrait = []
+    let foundPersonByTrait = {
         
-    return displayPeople(foundPeopleByTrait);
-}
+    }
+        
+    return displayPeople(foundPersonByTrait);
+}};
 
 /**
  * This function will be useful for STRINGIFYING a collection of person-objects
